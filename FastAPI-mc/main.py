d93149a1b28a4a11cf3e22e1a8e1c21f9586b633
threekd <threekd42@gmail.com>
2024-04-17 01:16:27 +0800

    link start

--- FastAPI-mc/main.py
+++ FastAPI-mc/main.py
@@ -1,39 +1,32 @@
 import docker
-import json
 import re
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-client = docker.from_env()
+class predict_parameters(BaseModel):
 
-smiles_or_inchi_or_file = 'InChI=1S/C11H14N2/c1-12-7-6-9-8-13-11-5-3-2-4-10(9)11/h2-5,8,12-13H,6-7H2,1H3'
-prob_thresh = 0.001
-param_file = '/trained_models_cfmid4.0/[M+H]+/param_output.log'
-config_file = '/trained_models_cfmid4.0/[M+H]+/param_config.txt'
-annotate_fragments = 0
-output_file_or_dir = 'output'
-apply_postproc = 1
-suppress_exceptions = 0
+    smiles_or_inchi_or_file: str
+    prob_thresh: int
+    param_file: str
+    config_file: str
+    annotate_fragments: int
+    output_file_or_dir: str
+    apply_postproc: int
+    suppress_exceptions: int
 
-command = f"cfm-predict 'CNCCC1=CNC2=CC=CC=C21' 0.001 /trained_models_cfmid4.0/[M+H]+/param_output.log /trained_models_cfmid4.0/[M+H]+/param_config.txt 1"
-container_output = client.containers.run('wishartlab/cfmid:latest', command, remove=True)
 
-decoded_output = container_output.decode('utf-8')
-
-sections = decoded_output.strip().split('\n\n')
-first_section = sections[0]
-second_section = sections[1]
 
 def decoding_smiles(section):
 
     pattern = r'(\d+)\s([\d.]+)\s([A-Za-z0-9#\+\(\)\[\]\=]+)'
     matches = re.findall(pattern, section)
 
-    json_data = []
+    smiles_data = []
     for match in matches:
         index, mol_weight, smiles = match
-        json_data.append({"index": int(index), "molecular_weight": float(mol_weight), "SMILES": smiles})
-    return json_data
+        smiles_data.append({"index": int(index), "molecular_weight": float(mol_weight), "SMILES": smiles})
+    return smiles_data
 
 def decoding_energy(section):
 
@@ -51,26 +44,52 @@ def decoding_energy(section):
                 # Add any additional captures here...
             })
         energy_data[energy_level] = data_points
-    energy_data["structure"] = decoding_smiles(second_section)
     return energy_data
 
-def decoding_smiles(section):
 
-    pattern = r'(\d+)\s([\d.]+)\s([A-Za-z0-9#\+\(\)\[\]\=]+)'
-    matches = re.findall(pattern, section)
+def predict(smiles_or_inchi_or_file):
 
-    json_data = []
-    for match in matches:
-        index, mol_weight, smiles = match
-        json_data.append({"index": int(index), "molecular_weight": float(mol_weight), "SMILES": smiles})
-    return json_data
 
-#json_output = json.dumps(decoding_energy(first_section), indent=4)
+    client = docker.from_env()
+
+    #smiles_or_inchi_or_file = 'InChI=1S/C11H14N2/c1-12-7-6-9-8-13-11-5-3-2-4-10(9)11/h2-5,8,12-13H,6-7H2,1H3'
+    prob_thresh = 0.001
+    param_file = '/trained_models_cfmid4.0/[M+H]+/param_output.log'
+    config_file = '/trained_models_cfmid4.0/[M+H]+/param_config.txt'
+    annotate_fragments = 0
+    output_file_or_dir = 'output'
+    apply_postproc = 1
+    suppress_exceptions = 0
+
+    command = f"cfm-predict {smiles_or_inchi_or_file} 0.001 /trained_models_cfmid4.0/[M+H]+/param_output.log /trained_models_cfmid4.0/[M+H]+/param_config.txt 1"
+    container_output = client.containers.run('wishartlab/cfmid:latest', command, remove=True)
+
+    decoded_output = container_output.decode('utf-8')
+
+    sections = decoded_output.strip().split('\n\n')
+    energy_section = sections[0]
+    smiles_section = sections[1]
+
+    energy_data = decoding_energy(energy_section)
+    seiles_data = decoding_smiles(smiles_section)
+    output_data = energy_data
+    output_data["structure"] = seiles_data
+
+    return output_data
+
+
+#json_output = json.dumps(decoding_energy(energy_section), indent=4)
 
 """
 with open('energy_data.json', 'w') as f:
     f.write(json_output)
 """
+class Input_item(BaseModel):
+    smiles: str
+
+
+
+
 
 app = FastAPI()
 app.add_middleware(
@@ -81,6 +100,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root():
-    return decoding_energy(first_section)
+    return predict('InChI=1S/C11H14N2/c1-12-7-6-9-8-13-11-5-3-2-4-10(9)11/h2-5,8,12-13H,6-7H2,1H3')
+
+@app.post("/update")
+async def calculate(data: Input_item):
+    result = predict(data.smiles)  # Use the 'data' instance to access 'smiles'
+    return {"result": result}
--- vue-mc/src/views/pages/Empty.vue
+++ vue-mc/src/views/pages/Empty.vue
@@ -6,19 +6,34 @@ import axios from 'axios'
 import { ref } from 'vue'
 
 const msg = ref(null)
-const showBar = ref(false) // 新增：控制Bar组件显示的状态
+const showBar = ref(false) 
 
-const toggleBarVisibility = () => { // 定义方法切换Bar的可见性
+const toggleBarVisibility = () => { 
   showBar.value = true
+  sendMessage()
 }
 
-axios.get("/")
+
+const sendMessage = () => {
+  axios.post("/update", { 
+    smiles: "InChI=1S/C8H10N4O2/c1-10-4-9-6-5(10)7(13)12(3)8(14)11(6)2/h4H,1-3H3"
+    // 如果模型需要其他字段，请确保它们在这里发送
+  })
   .then((res) => {
-    msg.value = res.data; 
+    msg.value = res.data.result // Corrected this line
+    console.log(res.data);
   })
   .catch((error) => {
-    console.error("Error fetching data:", error);
+    console.error(error.response.data);  // 打印错误信息，它会包括验证错误内容
   });
+};
+
+const getMessage = () => {
+  axios.get("/")
+    .then((res) => {
+      msg.value = res.data; // 更新响应式引用的值
+    });
+};
 
 </script>
 
@@ -28,7 +43,8 @@ axios.get("/")
       <Button label="Submit" class="mr-2 mb-2" @click="toggleBarVisibility"></Button>
   </div>
   <Form />
-  <Bar v-if="showBar" :energyData="Energy_data.energy1" :a="1"/>
+  <Bar v-if="showBar" :energyData="msg.energy1" :a="1"/>
   <Bar v-if="showBar && msg" :energyData="msg.energy2" :a="1"/>
+
 
 </template>
