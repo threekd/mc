9f236440622181ae7adad743cb17acaf57ce62e5
threekd <threekd42@gmail.com>
2024-04-18 04:08:51 +0800

    add rdkit

--- vue-mc/src/views/pages/SFC-mc/List.vue
+++ vue-mc/src/views/pages/SFC-mc/List.vue
@@ -1,11 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import RDKit from './RDKit-Vue/ExampleSVG.vue'
 import Energy_data_json from '../data/energy_data.json'
 
-const dataviewValue = ref(Energy_data_json)
-
-const molecular_weight = dataviewValue.map(data => data["molecular_weight"].toFixed(2));
-const SMILES = dataviewValue.map(data => data.SMILES);
+const dataviewValue = ref(Energy_data_json.structure)
 
 const layout = ref('grid');
 const sortKey = ref(null);
@@ -31,69 +29,27 @@ const onSortChange = (event) => {
     }
 };
 
-const getSeverity = (product) => {
-    switch (product.inventoryStatus) {
-        case 'INSTOCK':
-            return 'success';
-
-        case 'LOWSTOCK':
-            return 'warning';
-
-        case 'OUTOFSTOCK':
-            return 'danger';
-
-        default:
-            return null;
-    }
-};
 </script>
 
 <template>
+    <RDKit />
     <div class="grid">
         <div class="col-12">
             <div class="card">
                 <h5>DataView</h5>
-                <DataView :value="dataviewValue" :layout="layout" :paginator="true" :rows="9" :sortOrder="sortOrder" :sortField="sortField">
-                    <template #header>
-                        <div class="grid grid-nogutter">
-                            <div class="col-6 text-left">
-                                <Dropdown v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="Sort By Price" @change="onSortChange($event)" />
-                            </div>
-                            <div class="col-6 text-right">
-                                <!--some button-->
-                            </div>
-                        </div>
-                    </template>
-
+                <DataView :value="dataviewValue" :layout="layout" :paginator="true" :rows="8" :sortOrder="sortOrder" :sortField="sortField">
                     <template #grid="slotProps">
                         <div class="grid grid-nogutter">
-                            <div v-for="(item, index) in slotProps.items" :key="index" class="col-12 sm:col-6 md:col-4 p-2">
+                            <div v-for="(item, index) in slotProps.items" :key="index" class="col-12 sm:col-6 md:col-6 p-2">
                                 <div class="p-4 border-1 surface-border surface-card border-round flex flex-column">
-                                    <div class="surface-50 flex justify-content-center border-round p-3">
-                                        <div class="relative mx-auto">
-                                            <img class="border-round w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" style="max-width: 300px" />
-                                            <Tag :value="item.inventoryStatus" :severity="getSeverity(item)" class="absolute" style="left: 4px; top: 4px"></Tag>
-                                        </div>
-                                    </div>
                                     <div class="pt-4">
                                         <div class="flex flex-row justify-content-between align-items-start gap-2">
                                             <div>
-                                                <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
                                                 <div class="text-lg font-medium text-900 mt-1">{{ item.molecular_weight }}</div>
-                                            </div>
-                                            <div class="surface-100 p-1" style="border-radius: 30px">
-                                                <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
-                                                    <span class="text-900 font-medium text-sm">{{ item.rating }}</span>
-                                                    <i class="pi pi-star-fill text-yellow-500"></i>
-                                                </div>
                                             </div>
                                         </div>
                                         <div class="flex flex-column gap-4 mt-4">
                                             <span class="text-2xl font-semibold text-900">${{ item.SMILES }}</span>
-                                            <div class="flex gap-2">
-                                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto white-space-nowrap"></Button>
-                                                <Button icon="pi pi-heart" outlined></Button>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
---
+++ vue-mc/src/views/pages/SFC-mc/RDKit-Vue/ExampleSVG.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import MoleculeStructure from "./MoleculeStructure.vue";
+
+const molecules = ["CCCCC/C=C\C/C=C\C/C=C\C/C=C\CCCC(=O)N[C@H](C)CO"];
+</script>
+
+
+
+<template>
+  <div class="columns is-desktop">
+    <div class="column" v-for="(mol, idx) in molecules">
+      <MoleculeStructure
+        :id="`structure-example-svg-${idx}`"
+        :structure="mol"
+        :width="350"
+        :height="300"
+        svg-mode
+      ></MoleculeStructure>
+    </div>
+  </div>
+</template>
