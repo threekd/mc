a8fa2e6ea10319a9111f2db202defe48e152093a
threekd <threekd42@gmail.com>
2024-04-19 23:43:17 +0800

    update

--- FastAPI-mc/test-json.py
+++ FastAPI-mc/test-json.py
@@ -1,8 +1,6 @@
 import docker
 import re
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+import json
 
 def decoding_smiles(section):
 
@@ -34,24 +32,18 @@ def decoding_energy(section):
     return energy_data
 
 
-def predict(smiles_or_inchi_or_file):
+def predict():
 
 
     client = docker.from_env()
 
-    #smiles_or_inchi_or_file = 'InChI=1S/C11H14N2/c1-12-7-6-9-8-13-11-5-3-2-4-10(9)11/h2-5,8,12-13H,6-7H2,1H3'
-    prob_thresh = 0.001
-    param_file = '/trained_models_cfmid4.0/[M+H]+/param_output.log'
-    config_file = '/trained_models_cfmid4.0/[M+H]+/param_config.txt'
-    annotate_fragments = 0
-    output_file_or_dir = 'output'
-    apply_postproc = 1
-    suppress_exceptions = 0
-
+    smiles_or_inchi_or_file = 'InChI=1S/C11H14N2/c1-12-7-6-9-8-13-11-5-3-2-4-10(9)11/h2-5,8,12-13H,6-7H2,1H3'
     command = f"cfm-predict {smiles_or_inchi_or_file} 0.001 /trained_models_cfmid4.0/[M+H]+/param_output.log /trained_models_cfmid4.0/[M+H]+/param_config.txt 1"
     container_output = client.containers.run('wishartlab/cfmid:latest', command, remove=True)
 
     decoded_output = container_output.decode('utf-8')
+    with open('energy_data.txt', 'w') as f:
+        f.write(decoded_output)
 
     sections = decoded_output.strip().split('\n\n')
     energy_section = sections[0]
@@ -65,34 +57,8 @@ def predict(smiles_or_inchi_or_file):
     return output_data
 
 
-#json_output = json.dumps(decoding_energy(energy_section), indent=4)
+json_output = json.dumps(predict(), indent=4)
 
-"""
+
 with open('energy_data.json', 'w') as f:
     f.write(json_output)
-"""
-class Input_item(BaseModel):
-    smiles: str
-
-
-
-
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Only allow this origin
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-def read_root():
-    return predict('InChI=1S/C11H14N2/c1-12-7-6-9-8-13-11-5-3-2-4-10(9)11/h2-5,8,12-13H,6-7H2,1H3')
-
-@app.post("/update")
-async def calculate(data: Input_item):
-    result = predict(data.smiles)  # Use the 'data' instance to access 'smiles'
-    return {"result": result}
