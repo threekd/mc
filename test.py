7dd6a71931713f8a02342b78a690090289fe079e
threekd <threekd42@gmail.com>
2024-04-21 07:05:23 +0800

    save middle

--- test.py
+++ test.py
@@ -3,23 +3,49 @@ import re
 with open('energy_data.txt', 'r') as file:
     data = file.read()
 
-re_energy_all = re.compile(r'energy.*\n\n',flags=re.DOTALL)
-output = re_energy_all.search(data)
-output0= output.group(0)
-
-#print(output0)
-
-re_energy = re.compile(r'energy\d(.*?)energy\d(.*?)energy\d(.*?)\n\n(.*)',flags=re.DOTALL)
-
-energys = re_energy.search(data)
-"""
-print('energy0',energys.group(1))
-print('energy1',energys.group(2))
-print('energy2',energys.group(3))
-"""
-print('energy3',energys.group(4))
+def output_to_json1(output_str:str):
 
 
-re_stru = re.compile(r'(\d+)\s([\d.]+)\s(.+)\n',flags=re.MULTILINE)
-a = re_stru.findall(energys.group(4))
-print(a)
+    re_energy_and_fragment = re.compile(r'energy0(.*?)energy1(.*?)energy2(.*?)\n\n(.*)',flags=re.DOTALL)
+
+    energy_and_fragment = re_energy_and_fragment.search(output_str)
+    if energy_and_fragment:
+        energy0 = energy_and_fragment.group(1)
+        energy1 = energy_and_fragment.group(2)
+        energy2 = energy_and_fragment.group(3)
+        id_fragment = energy_and_fragment.group(4)
+
+        re_mass_intensity = re.compile(r'^([\d.]+)\s([\d.]+)', flags=re.MULTILINE)
+        re_fragment_id_value = re.compile(r'^[\d.]+\s[\d.]+\s(.*)', flags=re.MULTILINE)
+        re_fragment = re.compile(r'^([\d.]+)\s([\d.]+)\s(.*)', flags=re.MULTILINE)
+
+        mass_intensity0 = re_mass_intensity.findall(energy0)
+        mass_intensity1 = re_mass_intensity.findall(energy1)
+        mass_intensity2 = re_mass_intensity.findall(energy2)
+
+        fragment_id_value = re_fragment_id_value.findall(re_fragment_id_value)
+
+        fragment = re_fragment.findall(id_fragment)
+
+
+def output_to_json(output_str:str):
+
+    
+    re_energy_and_fragment = re.compile(r'energy0(.*?)energy1(.*?)energy2(.*?)\n\n(.*)',flags=re.DOTALL)
+
+    energy_and_fragment = re_energy_and_fragment.search(output_str)
+    if energy_and_fragment:
+        energys = energy_and_fragment.groups()[:3]
+        fragments = energy_and_fragment.groups()[3:4]
+
+        for energy in energys:
+            re_mass_intensity_other = re.compile(r'^([\d.]+)\s([\d.]+)\s(.*)', flags=re.MULTILINE)
+            mass_intensity_other = re_mass_intensity_other.findall(energy)
+            print(mass_intensity_other)
+
+
+
+        
+
+
+output_to_json(data)
