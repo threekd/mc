3ad1e56b3fa297629d0f704640fd5c168f103d24
threekd <threekd42@gmail.com>
2024-04-21 08:56:19 +0800

    json complete

--- test.py
+++ test.py
@@ -1,32 +1,8 @@
 import re
+import json
 
 with open('energy_data.txt', 'r') as file:
     data = file.read()
-
-def output_to_json1(output_str:str):
-
-
-    re_energy_and_fragment = re.compile(r'energy0(.*?)energy1(.*?)energy2(.*?)\n\n(.*)',flags=re.DOTALL)
-
-    energy_and_fragment = re_energy_and_fragment.search(output_str)
-    if energy_and_fragment:
-        energy0 = energy_and_fragment.group(1)
-        energy1 = energy_and_fragment.group(2)
-        energy2 = energy_and_fragment.group(3)
-        id_fragment = energy_and_fragment.group(4)
-
-        re_mass_intensity = re.compile(r'^([\d.]+)\s([\d.]+)', flags=re.MULTILINE)
-        re_fragment_id_value = re.compile(r'^[\d.]+\s[\d.]+\s(.*)', flags=re.MULTILINE)
-        re_fragment = re.compile(r'^([\d.]+)\s([\d.]+)\s(.*)', flags=re.MULTILINE)
-
-        mass_intensity0 = re_mass_intensity.findall(energy0)
-        mass_intensity1 = re_mass_intensity.findall(energy1)
-        mass_intensity2 = re_mass_intensity.findall(energy2)
-
-        fragment_id_value = re_fragment_id_value.findall(re_fragment_id_value)
-
-        fragment = re_fragment.findall(id_fragment)
-
 
 def output_to_json(output_str:str):
 
@@ -36,8 +12,9 @@ def output_to_json(output_str:str):
 
     energy_and_fragment = re_energy_and_fragment.search(output_str)
     if energy_and_fragment:
+        fragment_dict = {}
         energys = energy_and_fragment.groups()[:3]
-        fragments = energy_and_fragment.groups()[3:4]
+        fragment = energy_and_fragment.group(4)
 
         for num, energy in enumerate(energys):
             mass_intensity_other_dict = {}
@@ -45,16 +22,28 @@ def output_to_json(output_str:str):
             re_mass_intensity_other = re.compile(r'^([\d.]+)\s([\d.]+)\s(.*)', flags=re.MULTILINE)
             mass_intensity_other = re_mass_intensity_other.findall(energy)
             for item in mass_intensity_other:
+                other_dict = {}
                 mass = item[0]
                 intensity = item[1]
-                other = item[2]
-
-                mass_intensity_other_dict[f'{mass}'] = [intensity,other]
+                other = re.findall(r'[\d.]+',item[2])
+                for i in range(len(other)//2):
+                    other_dict[f'{other[i]}'] = other[len(other)//2+i]
+                mass_intensity_other_dict[f'{mass}'] = [intensity,other_dict]
             output_dict[f'energy{num}'] = mass_intensity_other_dict
+
+        re_id_mass_smiles = re.compile(r'^([\d]+)\s([\d.]+)\s(.*)', flags=re.MULTILINE)
+        id_mass_smiles = re_id_mass_smiles.findall(fragment)
+        for item in id_mass_smiles:
+            id = item[0]
+            mass = item[1]
+            smiles = item[2]
+            fragment_dict[f'{id}'] = [mass,smiles]
+        output_dict['fragment'] = fragment_dict
         print(output_dict)
 
+    output_json = json.dumps(output_dict, indent=4)
+    return output_json
 
+with open('energy_data.json', 'w') as f:
+    f.write(output_to_json(data))
         
-
-
-output_to_json(data)
