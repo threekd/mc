3bc5f79a8feefe5c2b31b4b60c8e1d767b7c502b
threekd <threekd42@gmail.com>
2024-04-25 04:33:25 +0800

    add pre

--- vue-mc/src/views/pages/SFC-mc/JSME.vue
+++ vue-mc/src/views/pages/SFC-mc/JSME.vue
@@ -1,11 +1,13 @@
 <template>
   <div ref="jsmeContainer" style="width: 100%; height: 100%;"></div>
+  <pre>{{ smiles }}</pre>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 
 const jsmeContainer = ref(null);
+const smiles = ref('');
 let jsmeApplet;
 
 onMounted(() => {
@@ -24,7 +26,15 @@ function initJsme() {
   const id = `jsme-container-${Math.random().toString(36).substr(2, 9)}`;
   jsmeContainer.value.id = id;
   jsmeApplet = new JSApplet.JSME(id, "100%", "100%", {
-    options: "fullScreenIcon",
+    options: "fullScreenIcon,query,hydrogens",
   });
+
+  // 监听结构改变事件来更新SMILES
+  jsmeApplet.setAfterStructureModifiedCallback(getSmilesFromJSME);
+}
+
+function getSmilesFromJSME() {
+  // 获取SMILES字符串并更新响应式属性
+  smiles.value = jsmeApplet.smiles();
 }
 </script>
