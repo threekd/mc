10674ef2fdbb5a16a25a8540436ebb3da1bc7a3a
threekd <threekd42@gmail.com>
2024-04-25 01:17:32 +0800

    add dialog

---
+++ vue-mc/src/views/pages/SFC-mc/Dialog.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+import JSME from './JSME.vue'
+
+const display = ref(false);
+
+const open = () => {
+    display.value = true;
+};
+const close = () => {
+    display.value = false;
+};
+
+</script>
+
+<template>
+    <div class="grid">
+        <div class="col-12 lg:col-6">
+            <div class="card p-fluid">
+                <h5>Dialog</h5>
+                <Dialog header="Dialog" v-model:visible="display" :breakpoints="{ '960px': '75vw' }" :style="{ width: '30vw' }" :modal="true">
+                    <div>
+                        <JSME />
+                    </div>
+                    <template #footer>
+                        <Button label="Ok" @click="close" icon="pi pi-check" class="p-button-outlined" />
+                    </template>
+                </Dialog>
+                <Button label="Show" icon="pi pi-external-link" style="width: auto" @click="open" />
+            </div>
+        </div>
+    </div>
+</template>
