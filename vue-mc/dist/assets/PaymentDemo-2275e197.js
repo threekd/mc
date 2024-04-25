a5e06b61b968a5a33e945be3b1af9868565bfe7a
threekd <threekd42@gmail.com>
2024-04-26 02:42:28 +0800

    update dict

---
+++ vue-mc/dist/assets/PaymentDemo-2275e197.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,d as o,e}from"./index-c758f327.js";const n={},c={class:"flex align-items-center py-5 px-3"},a=e("i",{class:"pi pi-fw pi-money-bill mr-2 text-2xl"},null,-1),i=e("p",{class:"m-0 text-lg"},"Payment Component Content via Child Route",-1),_=[a,i];function l(r,m){return s(),o("div",c,_)}const p=t(n,[["render",l]]);export{p as default};
