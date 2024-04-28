ba55edf3fcc9c611aef42a8aede50358a5903172
threekd <threekd42@gmail.com>
2024-04-28 21:51:44 +0800

    update link

--- vue-mc/src/views/Start.vue
+++ vue-mc/src/views/Start.vue
@@ -47,9 +47,9 @@ const block4 = ref(` <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
                         <span class="block text-6xl font-bold mb-1">Create the screens your</span>
                         <div class="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
                         <p class="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-                        <Button label="Learn More" type="button" class="mr-3 p-button-raised"></Button>
-                        <Button label="Live Demo" type="button" class="p-button-outlined"></Button>
+                        <router-link to="/cfmid" class="layout-topbar-logo">
+                            <Button label="Learn More" type="button" class="mr-3 p-button-raised"></Button>
+                        </router-link>
                     </section>
                 </div>
                 <div class="col-12 md:col-6 overflow-hidden">
