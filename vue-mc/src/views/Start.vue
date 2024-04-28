5679d82daef93f9b2dc485ad9b2bd244fdd96a31
threekd <threekd42@gmail.com>
2024-04-28 22:01:23 +0800

    update text

--- vue-mc/src/views/Start.vue
+++ vue-mc/src/views/Start.vue
@@ -1,52 +1,34 @@
 <script setup>
 import { ref } from 'vue';
-const block1 = ref(` <div class="grid grid-nogutter surface-section text-800">
-    <div class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
-        <section>
-            <span class="block text-6xl font-bold mb-1">Create the screens your</span>
-            <div class="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
-            <p class="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-            <Button label="Learn More" type="button" class="mr-3 p-button-raised"></Button>
-            <Button label="Live Demo" type="button" class="p-button-outlined"></Button>
-        </section>
-    </div>
-    <div class="col-12 md:col-6 overflow-hidden">
-        <img src="/demo/images/blocks/hero/hero-1.png" alt="Image" class="md:ml-auto block md:h-full" style="clip-path: polygon(8% 0, 100% 0%, 100% 100%, 0 100%)">
-    </div>
- </div>`);
-
-const block4 = ref(` <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
-    <div class="text-700 text-center">
-        <div class="text-blue-600 font-bold mb-3"><i class="pi pi-discord"></i>&nbsp;POWERED BY DISCORD</div>
-        <div class="text-900 font-bold text-5xl mb-3">Analytical Science AI</div>
-        <div class="text-700 text-2xl mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi qus.</div>
-        <Button label="Join Now" icon="pi pi-discord" class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"></Button>
-    </div>
- </div>`);
 
 </script>
 
 <template>
     <div>
-        <BlockViewer header="Call to Action" :code="block4" free>
+        <BlockViewer header="Call to Action" free>
             <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
                 <div class="text-700 text-center">
-                    <div class="text-blue-600 font-bold mb-3"><i class="pi pi-discord"></i>&nbsp;POWERED BY DISCORD</div>
-                    <div class="text-900 font-bold text-5xl mb-3">Join Our Design Community</div>
-                    <div class="text-700 text-2xl mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos.</div>
+                    <div class="text-blue-600 font-bold mb-3"><i class="pi pi-heart"></i>&nbsp;AI ACROSS APTC</div>
+                    <div class="text-900 font-bold text-5xl mb-3">Analytical Science AI</div>
+                    <div class="text-700 text-2xl mb-5">
+                        We are inspired by the ability of AI to help tackle the grand challenges in Analytical Science. At APTC, Our scientists and chemists are shedding new light on the frontiers of Chemistry through breakthroughs in machine learning, cloud infrastructure, and data processing and analytics. Together, our aim is to bring new technology and innovation to the analytical community.
+                        Explore how teams at APTC are using AI to accelerate scientific discovery.
+                    </div>
                     <Button label="Join Now" icon="pi pi-discord" class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"></Button>
                 </div>
             </div>
         </BlockViewer>
 
-        <BlockViewer header="Hero" :code="block1" free>
+        <BlockViewer header="Hero" free>
             <div class="grid grid-nogutter surface-section text-800">
                 <div class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center">
                     <section>
-                        <span class="block text-6xl font-bold mb-1">Create the screens your</span>
-                        <div class="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
-                        <p class="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="text-6xl text-primary font-bold mb-3">
+                            Al based Spectra Prediction: from chemical structure to MS spectra
+                        </div>
+                        <p class="mt-0 mb-4 text-700 line-height-3">
+                            Another tool is just in the opposite way, from the given chemical structureto predict the MS spectra by Al, because million compounds from Metlin arein the training set.The consistency between Al-predicted spectra andexperimental data provided substantial support to our analysis
+                        </p>
                         <router-link to="/cfmid" class="layout-topbar-logo">
                             <Button label="Learn More" type="button" class="mr-3 p-button-raised"></Button>
                         </router-link>
