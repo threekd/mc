79262320142ab43bb77d5a204308bfed86548adf
threekd <threekd42@gmail.com>
2024-05-02 00:47:51 +0800

    del info

--- vue-mc/src/views/Start.vue
+++ vue-mc/src/views/Start.vue
@@ -5,23 +5,23 @@ import { ref } from 'vue';
 
 <template>
     <div>
-        <BlockViewer header="Call to Action" free>
+        <BlockViewer header="">
             <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
                 <div class="text-700 text-center">
                     <div class="text-blue-600 font-bold mb-3"><i class="pi pi-heart"></i>&nbsp;AI ACROSS APTC</div>
-                    <div class="text-900 font-bold text-5xl mb-3">Analytical Science AI</div>
-                    <div class="text-700 text-2xl mb-5 text-left">
+                    <div class="text-900 font-bold text-6xl mb-3">Analytical Science AI</div>
+                    <div class="text-700 text-4xl mb-5 text-left">
                         We are inspired by the ability of AI to help tackle the grand challenges in Analytical Science. At APTC, Our scientists and chemists are shedding new light on the frontiers of Chemistry through breakthroughs in machine learning, cloud infrastructure, and data processing and analytics. Together, our aim is to bring new technology and innovation to the analytical community.
                     </div>
-                    <div class="text-700 text-2xl mb-5 text-left">
+                    <div class="text-700 text-4xl mb-5 text-left">
                         Explore how teams at APTC are using AI to accelerate scientific discovery.
                     </div>
-                    <Button label="Join Now" icon="pi pi-discord" class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"></Button>
+                <!--    <Button label="Join Now" icon="pi pi-discord" class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"></Button>   -->
                 </div>
             </div>
         </BlockViewer>
 
-        <BlockViewer header="Hero" free>
+        <BlockViewer header="Chem information">
             <div class="grid grid-nogutter surface-section text-800">
                 <div class="col-12 md:col-6 overflow-hidden flex justify-content-center">
                     <img src="/demo/images/blocks/hero/hero-1.png" width="600" alt="Image" class="block md:h-full" style="clip-path: polygon(10% 0, 100% 0%, 90% 100%, 0 100%) " />
@@ -31,7 +31,7 @@ import { ref } from 'vue';
                         <div class="text-6xl text-primary font-bold mb-3">
                             Al based Spectra Prediction: from chemical structure to MS spectra
                         </div>
-                        <p class="mt-0 mb-4 text-700 line-height-3">
+                        <p class="mt-0 mb-4 text-700 line-height-3 text-2xl">
                             Another tool is just in the opposite way, from the given chemical structureto predict the MS spectra by Al, because million compounds from Metlin arein the training set.The consistency between Al-predicted spectra andexperimental data provided substantial support to our analysis
                         </p>
                         <router-link to="/cfmid" class="layout-topbar-logo">
